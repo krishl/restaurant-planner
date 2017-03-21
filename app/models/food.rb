226fa78972f454f715e14c4a1fe19e20ad17df53
Krishel Lasam <krishel.lasam@gmail.com>
2017-03-21 13:13:15 +0000

    add links to show pages

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,7 +1,5 @@
 class Food < ApplicationRecord
   has_many :restaurant_foods
   has_many :restaurants, through: :restaurant_foods
-  has_many :restaurants
-  has_many :users, through: :restaurants
-  accepts_nested_attributes_for :restaurant_foods, allow_destroy: true
+  validates_presence_of :name
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,14 +1,15 @@
 class Restaurant < ApplicationRecord
   belongs_to :user, optional: true
   has_many :restaurant_foods
-  has_many :foods, through: :restaurant_foods, dependent: :delete_all
+  has_many :foods, through: :restaurant_foods, dependent: :destroy
+  validates_presence_of :name
   accepts_nested_attributes_for :foods, :reject_if => proc {|attributes|
     attributes.all? {|k,v| v.blank?}}, allow_destroy: true
 
   def foods_attributes=(foods_attributes)
     foods_attributes.values.each do |foods_attribute|
       if foods_attribute[:name] != ""
-        food = Food.find_or_create_by(name: foods_attribute[:name])
+        food = Food.find_or_create_by(name: foods_attribute[:name], price: foods_attribute[:price])
         self.foods << food
       end
     end
