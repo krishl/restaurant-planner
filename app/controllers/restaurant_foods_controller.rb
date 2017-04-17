2889a9499a1768064d77985cab1e9dac0f2efa30
Krishel Lasam <krishel.lasam@gmail.com>
2017-04-17 18:16:53 +0000

    add delete functionality for individual menu items, correct views

--- app/controllers/restaurant_foods_controller.rb
+++ app/controllers/restaurant_foods_controller.rb
@@ -1,6 +1,6 @@
 class RestaurantFoodsController < ApplicationController
   def destroy
-    @restaurant_food = RestaurantFood.find_by(params[:id])
+    @restaurant_food = RestaurantFood.find(params[:id])
     @restaurant_food.destroy
     redirect_to user_restaurants_url(current_user), notice: 'Successfully deleted.'
   end
