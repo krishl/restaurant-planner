2cea1e3370ba7d4ab4d187e8530179ab418ffe3f
Krishel Lasam <krishel.lasam@gmail.com>
2017-04-28 03:20:32 +0000

    improve authentication and fix user's edit page for users logged in with github

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,2 +1,13 @@
 class RegistrationsController < Devise::RegistrationsController
+
+  protected
+  
+  def update_resource(resource, params)
+    if resource.provider == "github"
+      params.delete("current_password")
+      resource.update_without_password(params)
+    else
+      resource.update_with_password(params)
+    end
+  end
 end
